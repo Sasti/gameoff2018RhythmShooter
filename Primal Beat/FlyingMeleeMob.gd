a979ba69ddb843728a61da97c3390dc7ade82af7
Tardog <buckling.spring@mailbox.org>
2018-11-10 10:09:04 +0100

    Make the mob move towards the player on aggro

--- Primal Beat/FlyingMeleeMob.gd
+++ Primal Beat/FlyingMeleeMob.gd
@@ -1,7 +1,7 @@
 # A flying enemy unaffected by gravity.
 # Hovers in the air until the player enters its aggro range, then pursues the player.
 # When it manages to reach the player's attack range, it latches on, slowly draining their life.
-extends RigidBody2D
+extends KinematicBody2D
 
 # Whether the player has entered our aggro range
 var aggroed = false
@@ -13,7 +13,7 @@ var attacking = false
 var target = Vector2()
 
 # Movement parameters
-var speed = 200
+var speed = 15
 var velocity = Vector2()
 
 func _ready():
@@ -21,11 +21,11 @@ func _ready():
 	$MobHitArea.connect('area_entered', self, 'hit')
 	$MobAggroRange.connect('area_entered', self, 'aggro')
 
-func _process(delta):
+func _physics_process(delta):
 	velocity = (target.position - position).normalized() * speed
 
 	if aggroed:
-		pass
+		move_and_collide(velocity)
 
 func hit(area):
 	if area.name == 'PlayerShotHitArea':
