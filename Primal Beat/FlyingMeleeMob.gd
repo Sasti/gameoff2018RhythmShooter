90a6d2e7441da1eadc82bc979ab6259fd4cc879b
Tardog <buckling.spring@mailbox.org>
2018-11-10 08:40:38 +0100

    Give the mob a more appropriate name and description

--- Primal Beat/FlyingMeleeMob.gd
+++ Primal Beat/FlyingMeleeMob.gd
@@ -1,3 +1,6 @@
+# A flying enemy unaffected by gravity.
+# Hovers in the air until the player enters its aggro range, then pursues the player.
+# When it manages to reach the player's attack range, it latches on, slowly draining their life.
 extends RigidBody2D
 
 # Whether the player has entered our aggro range
@@ -16,7 +19,6 @@ var velocity = Vector2()
 func _ready():
 	target = get_node('../Traveler')
 	$MobHitArea.connect('area_entered', self, 'hit')
-	$MobAggroRange.connect('area_entered', self, 'aggroed')
 
 func _process(delta):
 	velocity = (target.position - position).normalized() * speed
@@ -26,6 +28,3 @@ func _process(delta):
 
 func hit(object):
 	queue_free()
-
-func aggroed(object):
-	aggroing = true
