824512d4ed8959f1ffd72ca9b88fa6b399f828be
Tardog <buckling.spring@mailbox.org>
2018-11-10 06:02:22 +0100

    Create hitbox and aggro range for player

--- Primal Beat/DummyMob.gd
+++ Primal Beat/DummyMob.gd
@@ -1,8 +1,6 @@
 extends RigidBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var attacking = false
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
