542ec8ed842e27a13733e48e0d95a7f5452a50b2
sathielemann <thielemann@babymarkt.de>
2018-11-10 13:15:51 +0100

    added test level with colliding tilemap

--- Primal Beat/Traveler.gd
+++ Primal Beat/Traveler.gd
@@ -5,6 +5,11 @@ extends KinematicBody2D
 # var b = "textvar"
 var shot
 
+# gravity represent the accelaration for the downwards direction
+var gravity = 3
+
+var speed = 5
+
 func _init():
 	shot = load("res://PlayerShot.tscn")
 
@@ -19,14 +24,28 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
+	var movementDelta = Vector2(0,0)
+	
 	if Input.is_action_pressed("ui_down"):
-		position.y += 1
+		movementDelta.y += speed
 	
 	if Input.is_action_pressed("ui_up"):
-		position.y -= 1
+		movementDelta.y -= speed
 		
 	if Input.is_action_pressed("ui_right"):
-		position.x += 1
+		movementDelta.x += speed
 		
 	if Input.is_action_pressed("ui_left"):
-		position.x -= 1
+		movementDelta.x -= speed
+
+	movementDelta.y += gravity
+	
+	var collision = move_and_collide(movementDelta)
+	
+	if collision != null:
+		# This is very clumsy and needs to be replaces.
+		# I will replace it with a propper implementation as soon as I understand the engine better.
+		# Probably something like acceleration + speed * delta etc. and clamp it to the max speed
+		movementDelta.y = 0
+		movementDelta.x = collision.remainder.x
+		move_and_slide(collision.remainder)
