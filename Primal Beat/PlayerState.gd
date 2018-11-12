b090ab4bf1cbe8259657bbef743641028f2dbb71
sathielemann <thielemann@babymarkt.de>
2018-11-12 22:08:26 +0100

    #6 implemented health <-> live mechanic

--- Primal Beat/PlayerState.gd
+++ Primal Beat/PlayerState.gd
@@ -4,7 +4,9 @@ extends Node
 # This is used as a singleton (AutoLoad) to connect the scene parts which change or react on the PlayerState
 
 export var live = 3
-export var health = 3
+
+var max_health = 3
+export var health = 1
 
 signal player_state_changed
 
@@ -12,5 +14,11 @@ signal player_state_changed
 func damage_player(damage):
 	health -= damage
 	print('took %d damage - %d hearts left' % [damage, health])
+
+	# Update the live status and supsequently the health status when the health reaches zero
+	if health <= 0:
+		live -= 1
+		if live > 0:
+			health = max_health
+
 	emit_signal("player_state_changed")
-	
