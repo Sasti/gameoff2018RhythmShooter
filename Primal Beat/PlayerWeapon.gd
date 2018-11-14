fde33299af79ede5f8a26cbd13d4ed25f7357c4a
Tardog <buckling.spring@mailbox.org>
2018-11-14 19:22:14 +0100

    Flip the weapon when player facing changes #17

--- Primal Beat/PlayerWeapon.gd
+++ Primal Beat/PlayerWeapon.gd
@@ -5,21 +5,26 @@ const BULLET_DELAY = 0.25
 var player
 var gameworld
 
+# Horizontal position relative to the player
+var anchorPoint
+
 var can_shoot = true
 var shot_timer
 var shot
 
 func _init():
-	shot = load("res://PlayerShot.tscn")
+	shot = load('res://PlayerShot.tscn')
 
 func _ready():
 	player = get_parent()
 	gameworld = get_node('/root')
 
+	anchorPoint = position.x
+
 	shot_timer = Timer.new()
 	shot_timer.wait_time = BULLET_DELAY
 	shot_timer.one_shot = true
-	shot_timer.connect("timeout", self, "on_timeout_complete")
+	shot_timer.connect('timeout', self, 'on_timeout_complete')
 
 	add_child(shot_timer)
 
@@ -28,12 +33,18 @@ func on_timeout_complete():
 	can_shoot = true
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_shoot"):
+	animation = 'idle'
+
+	if Input.is_action_just_pressed('ui_shoot'):
 		var shotInstance = shot.instance()
 		shotInstance.position = player.position
 		gameworld.add_child(shotInstance)
 
 	if player.facing == player.FACING_LEFT:
-		print('left')
+		position.x = -(anchorPoint)
+		flip_h = true
 	elif player.facing == player.FACING_RIGHT:
-		print('right')
+		position.x = anchorPoint
+		flip_h = false
+
+	play()
