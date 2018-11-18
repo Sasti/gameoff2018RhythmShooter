59d6e7859c1db1020975a583b397faa8fd31355c
sathielemann <thielemann@babymarkt.de>
2018-11-18 16:48:30 +0100

    #19 Implemented ladder in the scene * Added example usage * Added controls for the player character * added some rock tyles so the player has something to stand on.

--- Primal Beat/elements/Ladder.gd
+++ Primal Beat/elements/Ladder.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Sprite
 
 const player_interaction_script = preload("../scripts/player_interaction.gd")
 onready var player_interaction = player_interaction_script.new()
@@ -6,16 +6,10 @@ onready var player_interaction = player_interaction_script.new()
 signal player_entered
 signal player_left
 
-func _on_Ladder_area_entered(area):
+func _on_ladder_area_entered(area):
 	if player_interaction._is_player_area(area):
 		emit_signal("player_entered")
 
-func _on_Ladder_area_exited(area):
+func _on_ladder_area_exited(area):
 	if player_interaction._is_player_area(area):
 		emit_signal("player_left")
-
-func _is_player_area(area):
-	if area.name == "PlayerHitbox":
-		return true
-	else:
-		return false
