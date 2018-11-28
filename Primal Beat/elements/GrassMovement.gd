cb999f96b5f2e476c68de9d454e64759a67e6bd5
sathielemann <thielemann@babymarkt.de>
2018-11-28 18:17:35 +0100

    #19 fixed grass movement script to also work with sprites that use ractangles

--- Primal Beat/elements/GrassMovement.gd
+++ Primal Beat/elements/GrassMovement.gd
@@ -1,7 +1,10 @@
 extends Sprite
 
+# The size of the used texture. Needs to be an Vector2
+var spriteSize
+
+# The transformation applied in the editor. Should be 
 onready var baseTransform = get_transform()
-onready var textureSize = get_texture().get_size()
 onready var elementScale = get_scale()
 
 var deltaSum = 0.0
@@ -10,10 +13,18 @@ export var wobbleSpeed = 1.3
 export var wobbleDistance = 0.2
 export var wobbleDirection = 1
 
+func _ready():
+	if region_enabled == true:
+		# If the sprite uses a region, which means it shows a part of the texture in use, the spriteSize is actually the region size
+		spriteSize = region_rect.size
+	else:
+		# This is the usual case where the sprite just show the whole content of the sprite
+		spriteSize = get_texture().get_size()
+
 func _physics_process(delta):
 	deltaSum += delta
 	
-	var elementSize = Vector2(textureSize.x * elementScale.x, textureSize.y * elementScale.y)
+	var elementSize = Vector2(spriteSize.x * elementScale.x, spriteSize.y * elementScale.y)
 	
 	var transformUp = Transform2D()
 	transformUp = transformUp.translated( Vector2(0, elementSize.y / 2) )
